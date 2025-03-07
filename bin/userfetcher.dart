ff8d6005fcbf9b10820cf17a8c87b02f9ef1db5d
Nafis Farhan <farh0111@algonquinlive.com>
2025-03-07 13:14:54 -0500

    starter files

---
+++ bin/userfetcher.dart
@@ -0,0 +1,5 @@
+import 'package:userfetcher/userfetcher.dart' as userfetcher;
+
+void main(List<String> arguments) {
+  print('Hello world: ${userfetcher.calculate()}!');
+}
---
+++ test/userfetcher_test.dart
@@ -0,0 +1,8 @@
+import 'package:userfetcher/userfetcher.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
